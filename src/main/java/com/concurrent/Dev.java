3214723c4a253b324c19e5a1f56b53dfdd9ffa70
zws <zws_09@163.com>
2022-01-26 19:17:40 +0800

    devdev

--- src/main/java/com/concurrent/Dev.java
+++ src/main/java/com/concurrent/Dev.java
@@ -3,5 +3,10 @@ package com.concurrent;
 public class Dev {
     public static void main(String[] args) {
         System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
     }
 }
